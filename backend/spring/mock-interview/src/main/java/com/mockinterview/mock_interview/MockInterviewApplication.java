cd1480401bc939b6eb8bae7a6342052dfbd8a97a
Pappu Dey <deypappu144@gmail.com>
2026-07-13 12:26:35 +0530

    add backend

---
+++ backend/spring/mock-interview/src/main/java/com/mockinterview/mock_interview/MockInterviewApplication.java
@@ -0,0 +1,13 @@
+package com.mockinterview.mock_interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockInterviewApplication.class, args);
+	}
+
+}
