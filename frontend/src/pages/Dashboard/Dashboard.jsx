7986c0e339ea501e9ab0f4ffcab2b0b91942659c
Trisha banerjee <trishabanerjee755@gmail.com>
2026-07-26 00:59:11 +0530

    update the dashboard

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -24,10 +24,41 @@ function Dashboard() {
         <div className="card">
           <h3>120</h3>
           <p>Questions solved</p>
-        </div>
-      </div>
+        </div>    
     </div>
-  )
+  <div className="quick-actions">
+    <h2>Quick Actions</h2>
+    <div className="action-buttons">
+      <button>Start Mock Interview</button>
+      <button>Practice coding</button>
+      <button>View performance</button>
+      <button>upload Resume</button>
+    </div>
+  </div>
+  <div className="performance-section">
+  <h2>Performance Overview</h2>
+
+  <div className="performance-card">
+    <p>Interview Progress</p>
+
+    <progress value="75" max="100"></progress>
+
+    <span>75% Completed</span>
+  </div>
+</div>
+<div className="recent-activity">
+  <h2>Recent Activity</h2>
+
+  <ul>
+    <li>✅ Completed Java Mock Interview</li>
+    <li>✅ Solved 10 DSA Questions</li>
+    <li>✅ Uploaded Resume</li>
+    <li>✅ Attempted SQL Quiz</li>
+  </ul>
+</div>
+  </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
+
